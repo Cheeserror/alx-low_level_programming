d35020f5e4565ecb8ffdb56b0ec30ec8e1290280
Cheeserror <usiphondlovu@gmail.com>
2023-02-21 10:56:03 -0800

    1-last_digit

--- 1-last_digit.c
+++ 1-last_digit.c
@@ -1,12 +1,10 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
-
 /**
- * main - Prints the last digit of a randomly generated number
- *        and whether it is greater than 5, less than 6, or 0.
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -17,20 +15,15 @@ int main(void)
 
 	if ((n % 10) > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n",
-			n, n % 10);
+	printf("Last digit of %d is %d and is greater than 5\n", n, (n % 10));
 	}
-	else if ((n % 10) < 6 && (n % 10) != 0)
+	else if ((n % 10) == 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n",
-			n, n % 10);
+	printf("Last digit of %d is %d and is 0\n", n, (n % 10));
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is 0\n",
-			n, n % 10);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, (n % 10));
 	}
-
 	return (0);
-
 }
